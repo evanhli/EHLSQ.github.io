c345ff609434ba96f07fb3429fa61dc83178d60c
Evan Li <evan.li.2010@hotmail.com>
2016-08-18 21:32:31 -0400

    Basic visualization ready

--- scripts/visualizer.js
+++ scripts/visualizer.js
@@ -1,14 +1,8 @@
 var clientID = '01bb591b4637b19ade24864ab8ab2dfc';
 
 var loader;
-
-window.onload = function init () {
-  var player =  document.getElementById('player');
-  player.crossOrigin = "anonymous";
-
-  var audioSource = new SoundCloudAudioSource(player);
-
-}
+var canvas;
+var canvasCtx;
 
 $( "form" ).submit(function( event ) {
   var input = document.getElementById('myText');
@@ -19,6 +13,18 @@ $( "form" ).submit(function( event ) {
 
 });
 
+window.onload = function init () {
+
+  var player =  document.getElementById('player');
+  player.crossOrigin = "anonymous";
+
+  canvas = document.querySelector('.visualizer');
+  canvasCtx = canvas.getContext("2d");
+
+
+  var audioSource = new SoundCloudAudioSource(player);
+
+}
 
 var SoundCloudAudioSource = function ( player ) {
   var audioCtx = new (window.AudioContext || window.webkitAudioContext);
@@ -30,16 +36,42 @@ var SoundCloudAudioSource = function ( player ) {
 
   var streamData = new Uint8Array(128);
 
-  setInterval(function () {
+  // setInterval(function () {
+  //   analyser.getByteFrequencyData(streamData);
+  //   console.log(streamData);
+  //
+  // }, 20)
+
+  function draw() {
+    drawVisual = requestAnimationFrame(draw);
+
     analyser.getByteFrequencyData(streamData);
-    console.log(streamData);
-  }, 20)
+    var bufferLength = analyser.frequencyBinCount;
+
+    canvasCtx.fillStyle = 'rgb(0, 0, 0)';
+    canvasCtx.fillRect(0, 0, canvas.width, canvas.height);
+
+    var barWidth = (canvas.width / bufferLength) * 4;
+    var barHeight;
+    var x = 0;
+
+    for(var i = 0; i < bufferLength; i+=3) {
+
+      barHeight = Math.pow(streamData[i],1);
+
+      canvasCtx.fillStyle = 'rgb(' + (barHeight+100) + ', ' + (barHeight) + ' , 0 )';
+      canvasCtx.fillRect(x, canvas.height-barHeight/2, barWidth, barHeight * 5);
+
+      x += barWidth + 1;
+    }
+  };
+
+  draw();
 
   player.play();
 
 
 }
-
 
 var SoundCloudLoader = function ( player, track_url ) {
 
